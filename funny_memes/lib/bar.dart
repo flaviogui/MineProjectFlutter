5aa563a70ddba911ebc17ed537b1f8fb37ec8bd0
isayan <62907623+kolitero@users.noreply.github.com>
2023-05-30 14:49:59 -0300

    Update bar.dart

--- funny_memes/lib/bar.dart
+++ funny_memes/lib/bar.dart
@@ -1,53 +1,40 @@
 import "package:flutter/material.dart";
 
-class MyBar extends StatelessWidget implements PreferredSizeWidget{
-
-  @override
-  Size get preferredSize => Size.fromHeight(kToolbarHeight);
+class MyBar extends StatefulWidget implements PreferredSizeWidget{
 
   final bool home;
   const MyBar({super.key,this.home = false});
+
+  @override
+  State<MyBar> createState() => _MyBarState();
+
+  @override
+  Size get preferredSize => Size.fromHeight(kToolbarHeight);
+}
+
+class _MyBarState extends State<MyBar> {
+  late final cont;
   @override 
   Widget build(BuildContext context){
+    cont = context;
     return AppBar(
       title:Text("FUNNY MEMES"),
       actions: [
+        PopupMenuButton(
+          onSelected:(value){
+            if(value == 1) Navigator.pop(context);
+          },
+          itemBuilder: (BuildContext context) => [
+            const PopupMenuItem(
+              child:Icon(Icons.arrow_back,color:Colors.black),
+              value:1
+            ) ,
+            if (widget.home) const PopupMenuItem(
+              child:Icon(Icons.home,color:Colors.black)
+            ),
+          ]
+        )
       ]
     );
   }
 }
-
-class DropMenu extends StatefulWidget {
-  final List<String> list;
-  const DropMenu({super.key,required this.list});
-
-  @override
-  State<DropMenu> createState() => _DropMenuState();
-}
-
-class _DropMenuState extends State<DropMenu> {
-
-  @override
-  Widget build(BuildContext context) {
-    return DropdownButton<String>(
-      value: ":",
-      icon: const Icon(Icons.arrow_downward),
-      elevation: 16,
-      style: const TextStyle(color: Colors.deepPurple),
-      underline: Container(
-        height: 2,
-        color: Colors.deepPurpleAccent,
-      ),
-      onChanged:(value) {
-
-      },
-      items: widget.list.map<DropdownMenuItem<String>>((String value) {
-        return DropdownMenuItem<String>(
-          value: value,
-          child: Text(value),
-        );
-      }).toList(),
-    );
-  }
-}
-
