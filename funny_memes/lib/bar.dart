a1d4544366c724c3ce3efe84ffa7edde2fdba676
isayan <62907623+kolitero@users.noreply.github.com>
2023-05-31 18:10:05 -0300

    Update bar.dart

--- funny_memes/lib/bar.dart
+++ funny_memes/lib/bar.dart
@@ -1,40 +1,42 @@
 import "package:flutter/material.dart";
+import "logoname.dart";
 
-class MyBar extends StatefulWidget implements PreferredSizeWidget{
+class MyBar extends StatelessWidget implements PreferredSizeWidget{
 
   final bool home;
-  const MyBar({super.key,this.home = false});
-
-  @override
-  State<MyBar> createState() => _MyBarState();
+  const MyBar({super.key,this.home = true});
 
   @override
   Size get preferredSize => Size.fromHeight(kToolbarHeight);
-}
 
-class _MyBarState extends State<MyBar> {
-  late final cont;
   @override 
   Widget build(BuildContext context){
-    cont = context;
-    return AppBar(
-      title:Text("FUNNY MEMES"),
-      actions: [
-        PopupMenuButton(
-          onSelected:(value){
-            if(value == 1) Navigator.pop(context);
-          },
-          itemBuilder: (BuildContext context) => [
-            const PopupMenuItem(
-              child:Icon(Icons.arrow_back,color:Colors.black),
-              value:1
-            ) ,
-            if (widget.home) const PopupMenuItem(
-              child:Icon(Icons.home,color:Colors.black)
-            ),
-          ]
-        )
-      ]
+    return Container(
+      color:Colors.blue,
+      child:Row(
+        children: [
+          Spacer(),
+          LogoName(),
+          Spacer(),
+          PopupMenuButton(
+            onSelected:(value){
+              if(value == 1) Navigator.pop(context);
+              if(value == 2) Navigator.popAndPushNamed(context,"/");
+            },
+            itemBuilder: (BuildContext context) => [
+              const PopupMenuItem(
+                child:Icon(Icons.arrow_back,color:Colors.black),
+                value:1
+              ) ,
+              if (home) const PopupMenuItem(
+                child:Icon(Icons.home,color:Colors.black),
+                value:2
+              ),
+            ]
+          ),
+        ],
+      )
     );
   }
 }
+
