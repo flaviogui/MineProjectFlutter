6489a8cb29facb94a68399bf86666641e1b208f6
isayan <62907623+kolitero@users.noreply.github.com>
2023-06-01 14:38:46 -0300

    Update routes.dart

--- funny_memes/lib/routes.dart
+++ funny_memes/lib/routes.dart
@@ -1,7 +1,10 @@
 import "home.dart";
+import "edit.dart";
 import "selection.dart";
+
 
 final routes =  {
     "/": (context) => const Home(),
-    "/selection": (context) => const Selection()
+    "/selection": (context) => const Selection(),
+    "/edit":(context) => const Edit(),
 };
