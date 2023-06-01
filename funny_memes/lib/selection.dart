2cb742e7af298557692b4cb5f551f5fbd4801164
flaviogui <flavioglaydson-pc@hotmail.com>
2023-05-31 21:07:26 -0300

    Algumas alteração

--- funny_memes/lib/selection.dart
+++ funny_memes/lib/selection.dart
@@ -2,62 +2,50 @@ import "package:flutter/material.dart";
 import "components/bar.dart";
 import "api.dart";
 
-var api =Api();
+var api = Api();
 
-class Selection extends StatefulWidget{
+class Selection extends StatefulWidget {
   const Selection({super.key});
-  @override 
+  @override
   State<StatefulWidget> createState() => _Selection();
 }
 
-class _Selection extends State<Selection>{
-  List<Widget> items = [
-    
-  ];
+class _Selection extends State<Selection> {
+  List<Widget> items = [];
   @override
-  Widget build(BuildContext){
+  Widget build(BuildContext) {
     items.add(TextButton(
-      child:Text("apertar aqui"),
-      onPressed:() => setState(() {
-        createItems();
-      })));
-    return  Scaffold(
-      appBar:const MyBar(),
-      body:SingleChildScrollView(
-        child:Column(
-          children:items
-        )
-      ) 
-    );
+        child: Text("apertar aqui"),
+        onPressed: () => setState(() {
+              createItems();
+            })));
+    return Scaffold(
+        appBar: const MyBar(),
+        body: SingleChildScrollView(child: Column(children: items)));
   }
 
-  Future<void> createItems() async{
+  Future<void> createItems() async {
     var list = await api.getMemes();
     List<Widget> rows = [];
-    for(int x=0;x<=10;x++){
+    for (int x = 0; x <= 10; x++) {
       List<Widget> row = [];
-      for(int y=0;y<=5;y++){
-          row.add(IconMeme(url:list[1]["url"]));
+      for (int y = 0; y <= 1; y++) {
+        row.add(IconMeme(url: list[x * 5 + y]["url"]));
       }
-      rows.add(Row(children:row));
+      rows.add(Row(children: row));
     }
     this.items.addAll(rows);
   }
 }
 
-class IconMeme extends StatelessWidget{
+class IconMeme extends StatelessWidget {
   final id;
   final url;
-  const IconMeme({super.key,this.id,this.url});
+  const IconMeme({super.key, this.id, this.url});
   @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
     return GestureDetector(
-      child: Container(
-        height:50,
-        width:50,
-        color:Colors.blue,
-        child:Image.network(url)
-      ),
+      child: Container(height: 50, width: 50, child: Image.network(url)),
     );
   }
 }
