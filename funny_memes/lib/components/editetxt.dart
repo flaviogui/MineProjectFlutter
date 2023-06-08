e7a74dcec2437b18d0f65118487d030a3a6260a2
flaviogui <flavioglaydson-pc@hotmail.com>
2023-06-07 21:19:29 -0300

    Update edit y editetxt

--- funny_memes/lib/components/editetxt.dart
+++ funny_memes/lib/components/editetxt.dart
@@ -1,63 +1,97 @@
 import "package:flutter/material.dart";
+import "../api.dart";
 import "mytextfield.dart";
 
-
-class EditText extends StatefulWidget{
+class EditText extends StatefulWidget {
+  final controler = TextEditingController();
+  final control = EditTextControler('', 0, 0, 0x000000, 0x000000);
   @override
   State<EditText> createState() => _EditTextState();
 }
 
 class _EditTextState extends State<EditText> {
   @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
     return Container(
-      decoration:BoxDecoration(
-        border:Border.all()
-      ), 
-      height:350,
-      width:300,
-      padding:EdgeInsets.all(4),
-      margin:EdgeInsets.all(4),
-      child:Column(
-        children: [
-          MyTextField(
-            hint:"insira o texto aqui",
-            controler:TextEditingController()
-          ),
+        decoration: BoxDecoration(border: Border.all()),
+        height: 350,
+        width: 300,
+        padding: EdgeInsets.all(4),
+        margin: EdgeInsets.all(4),
+        child: Column(children: [
+          MyTextField(hint: "insira o texto aqui", controler: widget.controler),
           Slider(
-            value:1,
-            min:0,
-            max:100,
-            label:"posicao x",
-            onChanged:(value){}
-          ),
+              value: widget.control.posX.toDouble(),
+              divisions: 100,
+              min: 0,
+              max: api.choiceX.toDouble(),
+              label: "posição X: ${widget.control.posX} ",
+              onChanged: (value) {
+                setState(() {
+                  widget.control.text = widget.controler.text;
+                  widget.control.posX = value.toInt();
+                });
+              }),
           Slider(
-            value:1,
-            min:0,
-            max:100,
-            label:"posicao y",
-            onChanged:(value){}
-          ),
+              value: widget.control.posY.toDouble(),
+              divisions: 100,
+              min: 0,
+              max: api.choiceY.toDouble(),
+              label: "posição Y: ${widget.control.posY} ",
+              onChanged: (value) {
+                setState(() {
+                  widget.control.text = widget.controler.text;
+                  widget.control.posY = value.toInt();
+                });
+              }),
           Slider(
-            value:0x000000,
-            min:0x000000,
-            max:0xffffff,
-            label:"cor do texto",
-            onChanged:(value){
-              setState((){
-                
-              });
-            }
-          ),
+              activeColor: Color(widget.control.textColor + 0xff000000),
+              value: widget.control.textColor.toDouble(),
+              min: 0x000000,
+              max: 0xffffff,
+              divisions: 0xffffff,
+              label:
+                  "cor do texto: ${widget.control.textColor.toRadixString(16)}",
+              onChanged: (value) {
+                setState(() {
+                  widget.control.text = widget.controler.text;
+                  widget.control.textColor = value.toInt();
+                });
+              }),
           Slider(
-            value:1,
-            min:0,
-            max:100,
-            label:"cor da borda",
-            onChanged:(value){}
-          )
-        ]
-      )
-    );
+              activeColor: Color(widget.control.borderColor + 0xff000000),
+              value: widget.control.borderColor.toDouble(),
+              min: 0x000000,
+              max: 0xffffff,
+              divisions: 0xffffff,
+              label:
+                  "cor da borda:${widget.control.borderColor.toRadixString(16)}",
+              onChanged: (value) {
+                setState(() {
+                  widget.control.text = widget.controler.text;
+                  widget.control.borderColor = value.toInt();
+                });
+              })
+        ]));
+  }
+}
+
+class EditTextControler {
+  String text;
+  int posX;
+  int posY;
+  int borderColor;
+  int textColor;
+  EditTextControler(
+      this.text, this.posX, this.posY, this.borderColor, this.textColor);
+
+  Map toMap() {
+    return {
+      'text': text,
+      'x': posX.toString(),
+      'y': posY.toString(),
+      'color': '#${textColor.toRadixString(16)}',
+      'outline_color': '#${borderColor.toRadixString(16)}'
+    };
   }
 }
--- funny_memes/lib/edit.dart
+++ funny_memes/lib/edit.dart
@@ -5,9 +5,7 @@ import "components/controleredit.dart";
 import "components/mytextfield.dart";
 import "api.dart";
 
-
-
-class Edit extends StatefulWidget{
+class Edit extends StatefulWidget {
   const Edit({super.key});
 
   @override
@@ -18,34 +16,31 @@ class _EditState extends State<Edit> {
   final controlerEdit = ControlerEdit();
 
   @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
     return ValueListenableBuilder(
-      valueListenable:controlerEdit.person,
-      builder:(_,value,__){
-      return Scaffold(
-        appBar: MyBar(),
-        body:ListView(
-          children:[
-            SizedBox(
-              height:300,
-              width:200,
-              child: Hero(
-                tag:api.choiceId,
-                child: ValueListenableBuilder(
-                  valueListenable:api.urlEdit,
-                  builder:(_,value,__) => Image.network(controlerEdit.edit ? value :api.choiceUrl
-                  ),
-                ),
-              )),
-              value ? EditPerson() : EditNormal()
-          ]
-        )
-        );}
-    );
+        valueListenable: controlerEdit.person,
+        builder: (_, value, __) {
+          return Scaffold(
+              appBar: MyBar(),
+              body: ListView(children: [
+                SizedBox(
+                    height: 300,
+                    width: 200,
+                    child: Hero(
+                      tag: api.choiceId,
+                      child: ValueListenableBuilder(
+                        valueListenable: api.urlEdit,
+                        builder: (_, va, __) => Image.network(
+                            controlerEdit.edit ? va : api.choiceUrl),
+                      ),
+                    )),
+                value ? EditPerson() : EditNormal()
+              ]));
+        });
   }
 }
 
-class EditNormal extends StatefulWidget{
+class EditNormal extends StatefulWidget {
   @override
   State<EditNormal> createState() => _EditNormalState();
 }
@@ -53,61 +48,51 @@ class EditNormal extends StatefulWidget{
 class _EditNormalState extends State<EditNormal> {
   final controlerEdit = ControlerEdit();
 
-  @override 
-  Widget build(BuildContext context){
-    return Column(
-      children:[
-        MyTextField(
-          controler:controlerEdit.topControlerEdit,
-          hint:"texto superior"
-        ),
-        MyTextField(
-          controler:controlerEdit.bottomControlerEdit,
-          hint:"texto inferior"
-        ),
-        Buttons(),
-        OutlinedButton(
-          child:Text("PERSONALIZAR"),
-          onPressed:(){
-            setState((){
+  @override
+  Widget build(BuildContext context) {
+    return Column(children: [
+      MyTextField(
+          controler: controlerEdit.topControlerEdit, hint: "texto superior"),
+      MyTextField(
+          controler: controlerEdit.bottomControlerEdit, hint: "texto inferior"),
+      Buttons(),
+      OutlinedButton(
+          child: const Text("PERSONALIZAR"),
+          onPressed: () {
+            setState(() {
               controlerEdit.person.value = true;
             });
-          }
-        )
-      ]
-    );
+          })
+    ]);
   }
 }
 
-class EditPerson extends StatefulWidget{
+class EditPerson extends StatefulWidget {
   @override
   State<EditPerson> createState() => _EditPersonState();
 }
 
 class _EditPersonState extends State<EditPerson> {
   final controlerEdit = ControlerEdit();
-  @override 
-  Widget build(BuildContext context){
-    return Column(
-          children:[
-            OutlinedButton(
-              child:Text("adicionar texto personalizado"),
-              onPressed:(){
-                setState(() { controlerEdit.textController.add(EditText());});
-                
-              }
-            ),
-            Column(
-                children:controlerEdit.textController,
-            ),
-            Buttons(),
-          ]
-    );
+  @override
+  Widget build(BuildContext context) {
+    return Column(children: [
+      OutlinedButton(
+          child: const Text("ADICIONAR TEXTO PERSONALIZADO"),
+          onPressed: () {
+            setState(() {
+              controlerEdit.textController.add(EditText());
+            });
+          }),
+      Column(
+        children: controlerEdit.textController,
+      ),
+      Buttons(),
+    ]);
   }
 }
 
 class Buttons extends StatefulWidget {
-
   @override
   State<Buttons> createState() => _ButtonsState();
 }
@@ -115,24 +100,26 @@ class Buttons extends StatefulWidget {
 class _ButtonsState extends State<Buttons> {
   final controlerEdit = ControlerEdit();
   @override
-  Widget build(BuildContext context){
-    return Row(
-      mainAxisAlignment:MainAxisAlignment.center,
-      children:[
-        OutlinedButton(
-            child:Text("EDITAR"),
-            onPressed: (){
-                if(!controlerEdit.person.value && controlerEdit.topControlerEdit.text != "" && controlerEdit.topControlerEdit.text != ""){
-                  controlerEdit.edit = true;
-                  api.getImage(controlerEdit.topControlerEdit.text,controlerEdit.bottomControlerEdit.text);
-                }
+  Widget build(BuildContext context) {
+    return Row(mainAxisAlignment: MainAxisAlignment.center, children: [
+      OutlinedButton(
+          child: const Text("EDITAR"),
+          onPressed: () {
+            if (!controlerEdit.person.value &&
+                controlerEdit.topControlerEdit.text != "" &&
+                controlerEdit.topControlerEdit.text != "") {
+              controlerEdit.edit = true;
+              api.getImage(controlerEdit.topControlerEdit.text,
+                  controlerEdit.bottomControlerEdit.text);
+            } else if (controlerEdit.person.value &&
+                controlerEdit.textController.length > 0) {
+              controlerEdit.edit = true;
+              api.getPersonImage(controlerEdit.textController
+                  .map((value) => value.control.toMap())
+                  .toList());
             }
-        ),
-        OutlinedButton(
-            child:Text("SALVA"),
-            onPressed: (){}
-        ),
-      ]
-    );
+          }),
+      OutlinedButton(child: const Text("SALVA"), onPressed: () {}),
+    ]);
   }
 }
