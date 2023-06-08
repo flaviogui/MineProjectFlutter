30b933c22cb1be70b378dfff1da198fc8d9f7709
flaviogui <flavioglaydson-pc@hotmail.com>
2023-06-07 21:43:31 -0300

    Update controleEdit

--- funny_memes/lib/components/controleredit.dart
+++ funny_memes/lib/components/controleredit.dart
@@ -1,30 +1,21 @@
 import "package:flutter/material.dart";
 import "editetxt.dart";
 
-class ControlerEditText{
-  var textColor =  0x000000;
-  var textBorderColor =  0x000000;
-  var posx = 0;
-  var posy = 0;
-  var textController = TextEditingController();
-}
-
-class ControlerEdit{
+class ControlerEdit {
   final ValueNotifier<bool> person = ValueNotifier(false);
   final bottomControlerEdit = TextEditingController();
   bool edit = false;
   final topControlerEdit = TextEditingController();
   List<EditText> textController = [];
+  List<EditTextControler> textEditsValue = [];
   static final ControlerEdit _instace = ControlerEdit._internal();
 
-  factory ControlerEdit(){
+  factory ControlerEdit() {
     return _instace;
   }
-  ControlerEdit._internal() {
-    
-  }
+  ControlerEdit._internal() {}
 
-  void  reset(){
+  void reset() {
     person.value = false;
     bottomControlerEdit.text = '';
     edit = false;
