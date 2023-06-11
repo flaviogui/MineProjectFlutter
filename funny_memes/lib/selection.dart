f0cd96b2414d912f9eb594fd97a2353c2788e832
flaviogui <flavioglaydson-pc@hotmail.com>
2023-06-11 14:19:08 -0300

    Melhorando o carregamento

--- funny_memes/lib/selection.dart
+++ funny_memes/lib/selection.dart
@@ -3,84 +3,73 @@ import "components/bar.dart";
 import "api.dart";
 import "components/controleredit.dart";
 
-
-
-class Selection extends StatefulWidget{
+class Selection extends StatefulWidget {
   const Selection({super.key});
-  @override 
+  @override
   State<StatefulWidget> createState() => _Selection();
 }
 
-class _Selection extends State<Selection>{
-  
-  @override 
+class _Selection extends State<Selection> {
+  @override
   void initState() {
-      api.getMemes();
-      super.initState();
-    }
+    api.getMemes();
+    super.initState();
+  }
 
   @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
     return Scaffold(
-      appBar:MyBar(),
-      body:ListView.builder(
-        itemCount:100,
-        itemBuilder:(context,index){
-          var url = "";
-          var id = "";
-          return IconMeme(
-            url:url,
-            id:id,
-            index:index
-            );
-          }
-      )
-    );
+        appBar: MyBar(),
+        body: ListView.builder(
+            itemCount: 100,
+            itemBuilder: (context, index) {
+              var url = "";
+              var id = "";
+              return IconMeme(url: url, id: id, index: index);
+            }));
   }
 }
 
-class IconMeme extends StatelessWidget{
+class IconMeme extends StatelessWidget {
   final id;
   final url;
   final index;
-  final controler =  ControlerEdit();
-  IconMeme({super.key,this.id,this.url,this.index});
+  final controler = ControlerEdit();
+  IconMeme({super.key, this.id, this.url, this.index});
   @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
     return ValueListenableBuilder(
-        valueListenable:api.memes,
-        builder: (_,value,__) { 
+        valueListenable: api.memes,
+        builder: (_, value, __) {
           String id = this.id;
           String url = this.url;
-          if(api.memes.value.length > 4){
+          if (api.memes.value.length > 4) {
             url = api.memes.value[index]["url"];
             id = api.memes.value[index]["id"];
           }
           return GestureDetector(
-          child: Container(
-            child: url != "" ? Hero(child: Image.network(value[index]["url"]),tag:id) : Loading()),
-            onTap:() {
-            api.choiceUrl = url;
-            api.choiceId =  id;
-            controler.reset();
-            Navigator.pushNamed(context,"/edit");
-            }
-        );}
-      );
+              child: Container(
+                  child: url != ""
+                      ? Hero(child: Image.network(value[index]["url"]), tag: id)
+                      : Loading()),
+              onTap: () {
+                api.choiceUrl = url;
+                api.choiceId = id;
+                controler.reset();
+                Navigator.pushNamed(context, "/edit");
+              });
+        });
   }
 }
 
-
 class Loading extends StatefulWidget {
-  const Loading({super.key});
+  const Loading({Key? key}) : super(key: key);
 
   @override
-  State<Loading> createState() =>
-      _LoadingState();
+  _LoadingState createState() => _LoadingState();
 }
 
-class _LoadingState extends State<Loading>
-    with TickerProviderStateMixin {
+class _LoadingState extends State<Loading> with SingleTickerProviderStateMixin {
   late AnimationController controller;
   bool determinate = false;
 
@@ -88,11 +77,11 @@ class _LoadingState extends State<Loading>
   void initState() {
     controller = AnimationController(
       vsync: this,
-      duration: const Duration(seconds: 2),
+      duration: const Duration(seconds: 3),
     )..addListener(() {
         setState(() {});
       });
-    controller.repeat(reverse: true);
+    controller.forward(); // Inicia a animação do ícone de carregamento
     super.initState();
   }
 
@@ -106,11 +95,12 @@ class _LoadingState extends State<Loading>
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.all(5.0),
-      child: 
-        CircularProgressIndicator(
+      child: Center(
+        child: CircularProgressIndicator(
           value: controller.value,
           semanticsLabel: 'loading',
-        )
+        ),
+      ),
     );
   }
 }
