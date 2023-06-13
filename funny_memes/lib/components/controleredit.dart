377309dcc8245e1ba98354f91d043327e30fd6dd
isayan <62907623+kolitero@users.noreply.github.com>
2023-06-13 14:22:15 -0300

    Update controleredit.dart

--- funny_memes/lib/components/controleredit.dart
+++ funny_memes/lib/components/controleredit.dart
@@ -1,25 +1,34 @@
 import "package:flutter/material.dart";
 import "editetxt.dart";
 
-class ControlerEdit {
+class ControlerEdit{
   final ValueNotifier<bool> person = ValueNotifier(false);
   final bottomControlerEdit = TextEditingController();
   bool edit = false;
   final topControlerEdit = TextEditingController();
+  List<TextEditingController> textsNormal = [];
   List<EditText> textController = [];
   List<EditTextControler> textEditsValue = [];
   static final ControlerEdit _instace = ControlerEdit._internal();
 
-  factory ControlerEdit() {
+  factory ControlerEdit(){
     return _instace;
   }
-  ControlerEdit._internal() {}
+  ControlerEdit._internal() {
+    
+  }
 
-  void reset() {
+  void  reset(){
+    textsNormal = [];
     person.value = false;
     bottomControlerEdit.text = '';
     edit = false;
     topControlerEdit.text = "";
     textController = [];
   }
+
+  void addNormalText(TextEditingController text){
+    textsNormal.add(text);
+  }
 }
+
